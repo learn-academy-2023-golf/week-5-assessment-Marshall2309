51add5cef3c9221092e323e4b2d9d1a2148278e0
Christopher <ckmarshal23@gmail.com>
2023-11-05 13:44:11 -0800

    Finished week 5 assessment

--- javascript-challenges.test.js
+++ javascript-challenges.test.js
@@ -14,6 +14,15 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+describe("string", () => {
+  it("takes in a string and returns a string with a coded message", () => {
+      expect(string(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(string(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(string(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -23,11 +32,56 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+const string = (str) => {
+    const array = []
+    str.split("").map(letter => {
+        if (letter === "a"){
+        array.push(4)
+     } else if (letter === "e" ||letter === "E"){
+        array.push(3)
+     } else if (letter === "i"){
+        array.push(1)
+     } else if (letter === "o"){
+        array.push(0)
+     } else {
+        array.push(letter)
+     }
+
+    })
+    
+    return array.join("")
+}
+
+//  "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+
 // Pseudo code:
+// I tested using the describe thing with expect statements.
+// In my function, I defined the string and used str as an argument.
+// I used the .split to split the array and the .map to iterate through every element.
+// If statements were used to target the letters individually and array.push was used to push in the specified number in the place of whatever letter was targeted.
+// array.join was used to push everything back into a string.
+
 
 // --------------------2) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+describe('isFullHouse', () => {
+    it('should return true for a full house hand', () => {
+        const hand1 = [5, 5, 5, 3, 3];
+        const hand4 = [7, 2, 7, 2, 7];
+
+        expect(isFullHouse(hand1)).to.be.true;
+        expect(isFullHouse(hand4)).to.be.true;
+    });
+
+    it('should return false for a non-full house hand', () => {
+        const hand2 = [5, 5, 3, 3, 4];
+        const hand3 = [5, 5, 5, 5, 4];
+
+        expect(isFullHouse(hand2)).to.be.false;
+        expect(isFullHouse(hand3)).to.be.false;
+    });
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -38,6 +92,29 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+console.log(isFullHouse(hand1)); 
+console.log(isFullHouse(hand2)); 
+console.log(isFullHouse(hand3)); 
+console.log(isFullHouse(hand4));
+
 // b) Create the function that makes the test pass.
 
+function isFullHouse(hand) {
+    const counts = {};
+    for (const num of hand) {
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    const values = Object.values(counts);
+    return (values.includes(2) && values.includes(3));
+}
+
 // Pseudo code:
+// Inside the describe block and we have two it statements. One for testing a full house hand and one for testing a non-full house hand.
+// Within the it block the test cases are defined using expect statements.
+// In the first it block, we expect isFullHouse(hand1) to be true and isFullHouse(hand4) to be true.
+// In the second it block, we expect isFullHouse(hand2) to be false and isFullHouse(hand3) to be false.
+// The isFullHouse function takes an array hand as input.
+// Inside the function, it initializes an empty object counts to keep track of the count of each number in the array.
+// It then iterates through the hand array and updates the counts in the counts object.
+// Next, it extracts the values (counts) from the counts object and checks if there is a count of 2 and a count of 3, which would indicate a full house.
+// If both conditions are met, the function returns true, otherwise it returns false.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -4,18 +4,103 @@
 # -------------------1) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+def sorted_values(hash)
+    values = hash.values.flatten
+    sorted_values = values.sort
+    return sorted_values
+  end
+  
+
+us_states = { 
+    northwest: ['Washington', 'Oregon', 'Idaho'], 
+    southwest: ['California', 'Arizona', 'Nevada'], 
+    notheast: ['Maine', 'New Hampshire', 'Rhode Island'] 
+}
+
+sorted_values_array = sorted_values(us_states)
+puts sorted_values_array
+
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
-# Pseudo code:
+# Pseudo code: 
+# First I extracted all the values from the hash into a new array called sorted_values. 
+# Then I used the flatten method to get the values into one array and get them to print alphabetically.
+# In the return, I callled for the sorted_values.
+# I then called the sorted_values array and the us_states together (can't remember the terminology) to create sorted_values_array.
+# I then printed out the array.
+
+
+
+
 
 # --------------------2a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+
+    def initialize(model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is currently traveling at #{@current_speed} mph."
+    end
+end
+
+bike1 = Bike.new("Mountain Bike")
+puts bike1.bike_info
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.
 
 # Pseudo code:
+# I created a class called Bike and initialized with a model, wheels, and current_speed per the instructions.
+# The attr_accessor was added to create getter and setter methods for the attributes.
+# the first def was used to initialize the model and the second def was used to create a method for the bike info. 
+# The model and wheels instance variables were interpolated under bike_info, to spit out a new sentence in a string.
+# bike1 line creates a new instance of the Bike class with the model name "Mountain Bike" and assigns it to the variable bike1.
+# puts bike1.bike_info: This line calls the bike_info method on the bike1 object and prints the returned string to the console.
+
+
 
 # -------------------2b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+
+    def initialize(model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is currently traveling at #{@current_speed} mph."
+    end
+
+    def pedal_faster(speed)
+        @current_speed = @current_speed + speed
+    end
+
+    def brake(speed)
+        if @current_speed > 0 
+            @current_speed = @current_speed - speed
+            if @current_speed <= 0
+                @current_speed = 0
+            end
+            @current_speed
+        end
+    end
+
+end
+
+bike1 = Bike.new("Mountain Bike")
+puts bike1.bike_info
+puts bike1.pedal_faster 10
+puts bike1.pedal_faster 18
+puts bike1.brake 5
+puts bike1.brake 25
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
@@ -23,3 +108,5 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(25) => 0
 
 # Pseudo code:
+# For this, I just added the pedal_faster method and the brake method with some tricky math to add the funtionality.
+# 
